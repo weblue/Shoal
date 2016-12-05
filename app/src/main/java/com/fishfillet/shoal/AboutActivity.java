1069a59ddfd5b38921f328252bc6c296afa653fe
stephen <stephenriblet@gmail.com>
2016-12-04 22:46:33 -0500

    About Page is commited

--- app/src/main/java/com/fishfillet/shoal/AboutActivity.java
+++ app/src/main/java/com/fishfillet/shoal/AboutActivity.java
@@ -1,8 +1,17 @@
 package com.fishfillet.shoal;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 /**
  * Created by TravisNguyen on 12/4/16.
  */
 
-public class AboutActivity {
+public class AboutActivity extends BaseActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.activity_about);
+        super.onCreate(savedInstanceState);
+    }
+
 }
